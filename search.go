f7869e9399953e95767586362ea0986bd05bd07e
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-13 17:33:56 +0300

    Add search and alphabeta functions (наконец-то)

--- search.go
+++ search.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
+const Infinite = 30000
+const Mate = 29000
+
 // IsRepetition reports whether the current position has occurred before
 // since the last irreversible move (capture, pawn move, or loss of castling
 // rights), which resets the fifty-move counter. Only that window of history
@@ -12,4 +17,108 @@ func IsRepetition(pos *Board) bool {
 		}
 	}
 	return false
+}
+
+func ClearForSearch(pos *Board, info *SearchInfo) {
+	for index := 0; index < 13; index++ {
+		for index2 := 0; index2 < 120; index2++ {
+			pos.SearchHistory[index][index2] = 0
+		}
+	}
+
+	for index := 0; index < 13; index++ {
+		for index2 := 0; index2 < 120; index2++ {
+			pos.SearchKillers[index][index2] = 0
+		}
+	}
+
+	ClearPvTable(pos.PvTable)
+
+	pos.Ply = 0
+
+	info.StartTime = GetTimeMs()
+	info.Stopped = false
+	info.Nodes = 0
+}
+
+func SearchPosition(pos *Board, info *SearchInfo) {
+	bestMove := NoMove
+	bestScore := -Infinite
+	var pvMoves int
+
+	ClearForSearch(pos, info)
+
+	for currentDepth := 1; currentDepth <= info.Depth; currentDepth++ {
+		bestScore = AlphaBeta(-Infinite, Infinite, currentDepth, pos, info, true)
+
+		pvMoves = GetPvLine(currentDepth, pos)
+		bestMove = pos.PvArray[0]
+
+		fmt.Printf("Depth:%d score:%d move:%s nodes:%d ", currentDepth, bestScore, PrMove(bestMove), info.Nodes)
+
+		pvMoves = GetPvLine(currentDepth, pos)
+		fmt.Print("pv")
+		for pvNum := 0; pvNum < pvMoves; pvNum++ {
+			fmt.Printf(" %s", PrMove(pos.PvArray[pvNum]))
+		}
+		fmt.Println()
+	}
+}
+
+func AlphaBeta(alpha, beta, depth int, pos *Board, info *SearchInfo, doNull bool) int {
+	Assert(CheckBoard(pos), "board check failed")
+
+	if depth == 0 {
+		info.Nodes++
+		return EvalPosition(pos)
+	}
+
+	if IsRepetition(pos) || pos.FiftyMove >= 100 {
+		return 0
+	}
+
+	if pos.Ply > MaxDepth-1 {
+		return EvalPosition(pos)
+	}
+
+	info.Nodes++
+
+	list := &MoveList{}
+	GenerateAllMoves(pos, list)
+
+	legal := 0
+	oldAlpha := alpha
+	bestMove := NoMove
+	score := -Infinite
+
+	for moveNum := 0; moveNum <= list.Count; moveNum++ {
+		if !MakeMove(pos, list.Moves[moveNum].MoveInt) {
+			continue
+		}
+
+		legal++
+		score = -AlphaBeta(-beta, -alpha, depth-1, pos, info, true)
+		TakeMove(pos)
+
+		if score > alpha {
+			if score >= beta {
+				return beta
+			}
+			alpha = score
+			bestMove = list.Moves[moveNum].MoveInt
+		}
+	}
+
+	if legal == 0 {
+		if SqAttacked(pos.KingSq[pos.Side], pos.Side^1, pos) {
+			return -Mate + pos.Ply
+		}
+		return 0
+	}
+
+	if alpha != oldAlpha {
+		StorePvMove(pos, bestMove)
+	}
+
+	return alpha
 }
